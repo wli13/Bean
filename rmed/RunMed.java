26a250c50728d3787e8eda31c97bb0b5bd96a3b4
Stefan Tan <stefantan@Stefans-Air.fios-router.home>
2018-05-17 20:44:32 -0400

    added skeletons

---
+++ rmed/RunMed.java
@@ -0,0 +1,22 @@
+//Bean
+//APCS2 pd01
+//HW49
+//2018-05-16
+
+public class RunMed{
+
+    ALHeapMin lilVals;
+    APHeapMax bigVals;
+	
+    public RunMed{
+	lilVals =;
+	bigVals =;
+    }
+
+    public getMedian(){
+    }//end getMedian()
+
+    public add (Integer newVal){
+    }//end add
+
+}//end class
